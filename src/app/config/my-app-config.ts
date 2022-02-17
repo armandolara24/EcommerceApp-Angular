ed37556e0f11cda050bbda3802c599ce022c4b12
armandolara24 <armandolaramail@gmail.com>
2022-02-17 13:42:21 -0600

    User authentication added.

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,11 @@
+// we are making "default export" of this JSON
+export default {
+
+    oidc: {
+        clientId: '0oa3wsd48tyvwQ1MR5d7',
+        issuer: 'https://dev-76318129.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
