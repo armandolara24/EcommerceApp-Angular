4f86dff419361ec8184a43f2e1f067c3b14498a1
armandolara24 <armandolaramail@gmail.com>
2022-07-08 15:09:51 -0500

    Working without https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  luv2shopApiUrl: "http://50.19.65.82:8080/api",
-  luv2shopApiUrlLoginCallback: "http://50.19.65.82/login/callback",
+  luv2shopApiUrl: "http://armandolara.com:8080/api",
+  luv2shopApiUrlLoginCallback: "http://armandolara.com/login/callback",
 };
